fec26a94b097d5f519f4d48d108f41895020397d
adityadafe <desmondaditya@gmail.com>
2024-05-25 15:54:38 +0530

    feat: safe goroutines implemented

--- loadbal/main.go
+++ loadbal/main.go
@@ -4,102 +4,117 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"slices"
 	"sync"
 	"time"
 )
 
+type Server struct {
+	url      string
+	isActive bool
+}
+
+type Servers []*Server
+
 var (
-	allServers []int
-	mu         sync.RWMutex
+	servers Servers = []*Server{
+		&Server{url: "http://localhost:3000", isActive: true},
+		&Server{url: "http://localhost:3001", isActive: true},
+		&Server{url: "http://localhost:3002", isActive: true},
+	}
+
+	timeout time.Duration = time.Second * 2
+
+	activeSvr []*Server
+
+	mu sync.Mutex
+
+	curr int = -1
 )
 
-func LookActiveServer(mp *map[int]bool) {
+func healthChecks() {
 	for {
-		fmt.Println("periodic server search")
-		st := 3000
-		fails := 0
-
-		for fails != 2 {
-			str := fmt.Sprintf("http://localhost:%d/health", st)
-			_, err := http.Get(str)
+		for i, svr := range servers {
+			_, err := http.Get(svr.url + "/health")
 			if err != nil {
-				if _, ok := (*mp)[st]; ok {
-					delete((*mp), st)
+				svr.isActive = false
+				if slices.Contains(activeSvr, svr) {
+					fs := servers[:i]
+					ss := servers[i+1:]
+
+					activeSvr = []*Server{}
+					mu.Lock()
+
+					for _, f := range fs {
+						activeSvr = append(activeSvr, f)
+					}
+
+					for _, f := range ss {
+						activeSvr = append(activeSvr, f)
+					}
+					mu.Unlock()
 				}
-				fails++
-				st++
 				continue
 			}
-
-			(*mp)[st] = true
-			st++
-		}
-
-		time.Sleep(2 * time.Second)
-	}
-}
-
-func GetAllServer(mp *map[int]bool) {
-	for {
-		// all go routines can readconcurrently after placing read lock
-		mu.RLock()
-		if len(*mp) != 0 {
-			for k := range *mp {
-				allServers = append(allServers, k)
+			mu.Lock()
+			svr.isActive = true
+			if slices.Contains(activeSvr, svr) {
+				mu.Unlock()
+				continue
 			}
-			mu.RUnlock()
+			activeSvr = append(activeSvr, svr)
+			mu.Unlock()
 		}
-
-		time.Sleep(2 * time.Second)
-		allServers = []int{}
+		time.Sleep(timeout)
 	}
 }
 
-func GetCurrentRobin(curr *int) int {
-	*curr++
-	fmt.Println(*curr)
-	if *curr >= len(allServers) {
-		*curr = 0
+func GetCurrentRobin() *Server {
+	mu.Lock()
+	curr++
+	fmt.Println("Request forwarded to Server ", curr)
+	if curr >= len(activeSvr) {
+		curr = 0
 	}
-	robin := allServers[*curr]
+	robin := activeSvr[curr]
+	mu.Unlock()
 	return robin
+}
+
+func ForwardRequest(w http.ResponseWriter, r *http.Request, str string) {
+	proxyReq, err := http.NewRequest(r.Method, str, r.Body)
+	if err != nil {
+		http.Error(w, "Error creating request", http.StatusInternalServerError)
+	}
+	proxyReq.Header = r.Header.Clone()
+	proxyReq.Header.Set("X-Forwarded-For", r.RemoteAddr)
+
+	client := &http.Client{}
+	res, err := client.Do(proxyReq)
+
+	if err != nil {
+		http.Error(w, "Error forwarding request", http.StatusBadGateway)
+	}
+	defer res.Body.Close()
+
+	for header, values := range r.Header {
+		for _, value := range values {
+			proxyReq.Header.Add(header, value)
+		}
+	}
+
+	w.WriteHeader(res.StatusCode)
+	io.Copy(w, res.Body)
+
 }
 
 func main() {
 
-	var curr int = -1
-
-	mp := make(map[int]bool)
-
-	go LookActiveServer(&mp)
-	go GetAllServer(&mp)
+	go healthChecks()
 
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		robin := GetCurrentRobin(&curr)
-		str := fmt.Sprintf("http://localhost:%d", robin)
-		proxyReq, err := http.NewRequest(r.Method, str, r.Body)
-		if err != nil {
-			http.Error(w, "Error creating request", http.StatusInternalServerError)
-		}
-		proxyReq.Header = r.Header.Clone()
-		proxyReq.Header.Set("X-Forwarded-For", r.RemoteAddr)
-
-		client := &http.Client{}
-		res, err := client.Do(proxyReq)
-
-		if err != nil {
-			http.Error(w, "Error forwarding request", http.StatusBadGateway)
-		}
-		defer res.Body.Close()
-
-		for header, values := range r.Header {
-			for _, value := range values {
-				proxyReq.Header.Add(header, value)
-			}
-		}
-
-		w.WriteHeader(res.StatusCode)
-		io.Copy(w, res.Body)
+		robin := GetCurrentRobin()
+		ForwardRequest(w, r, robin.url)
 	})
 
 	http.ListenAndServe(":5173", nil)
