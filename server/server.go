429877073705697af88ec2856134c4a4b2ecce79
adityadafe <desmondaditya@gmail.com>
2024-05-25 23:26:19 +0530

    chore: logging

--- server/server.go
+++ server/server.go
@@ -29,7 +29,7 @@ func (s *Server) Run() {
 	sm.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		fmt.Print("Server hosted at ", port, " served request to ", r.UserAgent())
+		fmt.Println("Server hosted at ", port, " served request to ", r.UserAgent())
 		// converting port to int
 		pti, _ := strconv.Atoi(port[1:])
 		w.Write([]byte("Hello from server " + strconv.Itoa(pti-2999)))
